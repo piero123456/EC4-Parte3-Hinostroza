b4a454d9d87a6f782e6fbf77cb1ce1c8d66a99e1
Alfonso Contreras <1923010051@untels.edu.pe>
2022-07-26 08:56:53 -0500

    Parte 2 - Tabla Estudiante - OK

--- VCH_UC04/miapp/models.py
+++ VCH_UC04/miapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Estudiante(models.Model):
+    codigo = models.TextField(max_length=10)
+    dni = models.TextField(max_length=8)
+    nombre = models.TextField()
+    apepat = models.TextField()
+    apemat = models.TextField()
+    direccion = models.TextField()
+    telefono = models.TextField()
+    estado = models.CharField(max_length=1)
+
+ 
